b41a6af850643fb613aabaf63a3e411c292cd9c5
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-11-17 15:06:28 +0100

    added documentation for task and an aria label

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,18 @@
 /**
- * This is the task component.
+ * This is the task component, which represents each induvidual task that gets added. It displays the title and description and is creating the button to delete the task.
+ *
+ * @param {TaskProps} props - The props for the component
+ * @param {number} id - The unique identifier for each task
+ * @param {string} title - The title of the task
+ * @param {ReactNode} children - The content that gets displayed inside the task (discription)
+ * @param {(id: number)=>void} onDelete - A fuction for deletion, when the delete button is clicked.
+ *
+ * @returns {JSX.Element} - The rendered Task component.
+ * 
+ * What to add in the file?
+ * <Task id={task.id} title={task.title} onDelete={handleDelete}>
+    <p>{task.description}</p>
+   </Task>
  *
  */
 import { type ReactNode } from "react";
@@ -18,7 +31,12 @@ export default function Task({ id, title, children, onDelete }: TaskProps) {
         <h2>{title}</h2>
         {children}
       </div>
-      <button onClick={() => onDelete(id)}>Delete</button>
+      <button
+        onClick={() => onDelete(id)}
+        aria-label="{`Delete task: ${title}`}"
+      >
+        Delete
+      </button>
     </article>
   );
 }
