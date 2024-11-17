2f84d6c8dafc1dade59b71321159d5e3c5b646b9
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-11-17 14:39:42 +0100

    added documentation for taskmanager and a heading. Note - need to look into what type of headings to use for the page

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -1,5 +1,11 @@
 /**
+ * The Taskmanager component manages the tasks by allowing the users to add and delete tasks. It uses local storage to ensure the tasks are available after a page refresh.
  *
+ * What to add to the file?
+ * <Taskmanager />
+ *
+ * Note for future additions for the task files in general
+ * Making it possible to mark as complete, adding a event keydown for space or enter to enable keyboard usage for marking as complete.
  */
 import TaskList from "./TaskList.tsx";
 import { useEffect, useState } from "react";
@@ -14,18 +20,22 @@ export type Task = {
 export default function TaskManager() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  // Loads tasks from the local storage
   useEffect(() => {
     const storedTasks = localStorage.getItem("tasks");
     if (storedTasks) {
       setTasks(JSON.parse(storedTasks));
     }
   }, []);
+
+  // Stores the tasks from local storage
   useEffect(() => {
     if (tasks.length > 0) {
       localStorage.setItem("tasks", JSON.stringify(tasks));
     }
   }, [tasks]);
 
+  // handler for adding tasks
   function handleAddTask(task: string, summary: string) {
     setTasks((prevTasks) => {
       const newTask: Task = {
@@ -37,12 +47,14 @@ export default function TaskManager() {
     });
   }
 
+  //handles deletion for tasks based on id
   function handleDeleteTask(id: number) {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   }
 
   return (
     <main>
+      <h1 className="TM-h1">Taskmanager</h1>
       <NewTask onAddTask={handleAddTask} />
       <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} />
     </main>
