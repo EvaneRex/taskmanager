51fe44b682ec9d62be0fbb860d4d9644c498389e
AnitaSteffensen <anitanyholms@gmail.com>
2024-11-19 09:13:26 +0100

    Delete list that will not be used

--- src/components/CatagoryButton.tsx
+++
@@ -1,3 +0,0 @@
-/**
- * This is for each catagory, WIP meaning we might not need it but we probably will
- */
--- src/components/ExistingLists.tsx
+++
@@ -1,3 +0,0 @@
-/**
- * This is the component for the existing lists on the front page
- */
