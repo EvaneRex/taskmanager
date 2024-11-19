2db4dcf17e0c40c5310f6304439ac8865abdfac3
AnitaSteffensen <anitanyholms@gmail.com>
2024-11-19 09:10:23 +0100

    added comments to tsx files

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,11 @@
 /**
  * This is the task component, which represents each induvidual task that gets added. It displays the title and description and is creating the button to delete the task.
  *
- * @param {TaskProps} props - The props for the component
- * @param {number} id - The unique identifier for each task
- * @param {string} title - The title of the task
- * @param {ReactNode} children - The content that gets displayed inside the task (discription)
- * @param {(id: number)=>void} onDelete - A fuction for deletion, when the delete button is clicked.
+ * @param {TaskProps} props - The props for the component, it has been imported from the TaskList file. 
+ * @param {removeTaskItem} - A fuction for deletion, when the delete button is clicked.
  *
  * @returns {JSX.Element} - The rendered Task component.
- * 
- * What to add in the file?
- * <Task id={task.id} title={task.title} onDelete={handleDelete}>
-    <p>{task.description}</p>
-   </Task>
+ 
  *
  */
 
